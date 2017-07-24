40ee3527628c82b94f2743b55465f0599e8153b7
Baskoro Susanto <baskoro.jsc@gmail.com>
2017-07-24 16:01:36 +0700

    Updated run

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 
 sudo docker stop storage
 sudo docker rm storage
-sudo docker run -d --name="storage" -p -v /upload /var/www/cdn/v2 3301:3300 192.168.3.102:6000/storage-service
+sudo docker run -d --name="storage" -p 3301:3300 -v /upload /var/www/cdn/v2 192.168.3.102:6000/storage-service
